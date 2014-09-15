056e9daef9c0bdbf5db521def20d061d6189eb54
Kia Winslow <ksukop@gmail.com>
2014-09-15 14:42:47 -0700

    Bees are in the progress

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  // color <- from Bee;
+  // food <- from Grub;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  return this.honeyPot ++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  return this.honeyPot --;
+}
+// .eat <- from grub
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
 };
+
+ForagerBee.prototype = Object.create(RetiredForagerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+RetiredForagerBee.prototype.forage = function(){
+  return "I am too old, let me play cards instead";
+}
+
+RetiredForagerBee.prototype.gamble = 
+
 
